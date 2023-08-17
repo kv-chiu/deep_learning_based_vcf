88e10746962b687c0ec366e4a372d4572080ab4b
kv-chiu <kelvin.chiu021@outlook.com>
2023-08-17 17:27:05 +0800

    feat: update DataPreparationTool class

--- interfaces.py
+++ interfaces.py
@@ -1,10 +1,12 @@
 # 在 interfaces.py 中
 from abc import ABC, abstractmethod
+from typing import List, Union
+import subprocess
 import os
 
 class DataPreparationTool(ABC):
     def __init__(self):
-        self.current_dir = os.getwd()
+        self.current_dir = os.getcwd()
         self.cmd_list = []
         self.is_success = False
 
@@ -12,17 +14,32 @@ class DataPreparationTool(ABC):
     def process(self, input_data):
         pass
 
+    def start_cmd(self, cmd: str) -> None:
+        print('-' * 50)
+        print(f'执行命令：{cmd}')
+        print('-' * 50)
+
+    def end_cmd(self, cmd: str, if_success: bool) -> None:
+        print('-' * 50)
+        print(f'命令执行完毕：{cmd}')
+        if if_success:
+            print('命令执行成功。')
+        else:
+            print('命令执行失败。')
+        print('-' * 50)
+
     def run_cmd_list(self, arr: List[str]) -> Union[bool, None]:
         isSuccess = False
         for cmd in arr:
+            self.start_cmd(cmd)
             proc = subprocess.run(cmd, shell=True)
             if proc.returncode != 0:
                 isSuccess = False
-                print("命令执行失败。")
+                self.end_cmd(cmd, isSuccess)
                 break
             else:
                 isSuccess = True
-                print("命令执行成功。")
+                self.end_cmd(cmd, isSuccess)
         return isSuccess
 
 class DataFilterTool(ABC):
